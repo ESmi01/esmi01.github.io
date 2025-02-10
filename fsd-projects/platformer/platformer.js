19c79d41e426ce810b090ffaba6e0aca8798f462
ESmi01 <smith856@wearescpps.org>
2025-02-10 14:19:19 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,20 +31,27 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-    createPlatform(300, 500, 200, 10);
+    createPlatform(200, 500, 250, 10);
     createPlatform(500, 625, 300, 125);
     createPlatform(1150, 400, 250, 375);
     createPlatform(650, 375, 200, 10);
     createPlatform(660, 250, 180, 10);
+    createPlatform(300, 300, 10, 200);
+    createPlatform(975, 300, 50, 10);
+    createPlatform(0, 300, 500, 10);
+    createPlatform(0, 625, 100, 125);
+    createPlatform(300, 275, 200, 25);
     // TODO 3 - Create Collectables
     createCollectable("database", 1275, 300);
     createCollectable("database", 730, 175);
-    createCollectable("database", 1000, 650);
-    createCollectable("database", 380, 400);
+    createCollectable("database", 975, 650);
+    createCollectable("database", 350, 400);
+    createCollectable("database", 230, 400);
     // TODO 4 - Create Cannons
     createCannon("top", 1000, 1000);
     createCannon("left", 500, 1500);
     createCannon("bottom", 1000, 1000);
+    createCannon("right", 260, 1500);
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
